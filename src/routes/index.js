7b7c2e5832d0fe6e0a330be3467f7327f792459b
KDOE-117 <jdn2037@gmail.com>
2024-10-09 03:18:50 -0500

    API Basica Terminada

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,20 +12,68 @@ routes.get('/estudiantes', async (req, res) => {
     }
 });
 
-
 /*Get One*/
-routes.get('/About', (req, res) =>
-    res.render('About', { numero: 30 }))
-/*Create*/
-routes.post('/GenerarEstudiante', async (req, res) => {
+routes.get('/estudiantes/:codigoEstudiante', async (req, res) => {
+    try {
+        const estudiante = await Estudiante.findOne({
+            where: { codigoEstudiante: req.params.codigoEstudiante },
+        });
+        if (estudiante) {
+            res.status(200).json(estudiante);
+        } else {
+            res.status(404).json({ error: 'No se encontró al estudiante.' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
+/*Create*/
+routes.post('/generarEstudiante', async (req, res) => {
+    try {
+        const estudiante = await Estudiante.create(req.body);
+        console.log(`Estudiante creado: ${estudiante.nombre} ${estudiante.apellido}`);
+        res.status(201).json(estudiante);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 /*Update One*/
-routes.put('/About', (req, res) =>
-    res.render('About', { numero: 30 }))
+routes.put('/actualizarEstudiante/:codigoEstudiante', async (req, res) => {
+    try {
+        const estudiante = await Estudiante.findOne({
+            where: { codigoEstudiante: req.params.codigoEstudiante },
+        });
+        if (estudiante) {
+            await estudiante.update(req.body);
+            console.log(`Estudiante actualizado:`);
+            res.status(200).json(estudiante);
+        } else {
+            res.status(404).json({ error: 'No se encontró al estudiante.' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
 /*delete One*/
-routes.delete('/About', (req, res) =>
-    res.render('About', { numero: 30 }))
+routes.delete('/borrarEstudiante/:codigoEstudiante', async (req, res) => {
+    try {
+        const estudiante = await Estudiante.findOne({
+            where: { codigoEstudiante: req.params.codigoEstudiante },
+        });
+        if (estudiante) {
+            await estudiante.destroy();
+            console.log(`El Estudiante Ha Sido Eliminado!`);
+            res.status(204).send();
+        } else {
+            res.status(404).json({ error: 'Estudiante no encontrado' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 export default routes
